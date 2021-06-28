197f8428c211d42edd5cc5f73bbd80a3b9bcd7fe
EugeneKhomitskiy <mtl_87@list.ru>
2021-06-28 15:21:23 +0300

    change WallServiceTest.kt

--- src/test/kotlin/ru/netology/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -6,40 +6,135 @@ import org.junit.Assert.*
 
 class WallServiceTest {
 
-    private val post = Post(
-        id = 4,
-        owner_id = 111,
-        from_id = 222,
-        created_by = 333,
-        date = 4444,
-        text = " ",
-        reply_owner_id = 444,
-        reply_post_id = 555,
-        friends_only = true,
-        post_type = "post",
-        signer_id = 666,
-        can_pin = true,
-        can_delete = true,
-        can_edit = true,
-        is_pinned = true,
-        marked_as_ads = true,
-        is_favorite = true,
-        postponed_id = 777
-    )
-
     @Test
     fun add_Test() {
+        val post = Post(
+            id = 4,
+            owner_id = 1,
+            from_id = 2,
+            created_by = 3,
+            date = 2222,
+            text = " ",
+            reply_owner_id = 4,
+            reply_post_id = 5,
+            friends_only = true,
+            post_type = "post",
+            signer_id = 6,
+            can_pin = true,
+            can_delete = true,
+            can_edit = true,
+            is_pinned = true,
+            marked_as_ads = true,
+            is_favorite = true,
+            postponed_id = 7
+        )
         val service = WallService()
-        val posts = emptyArray<Post>()
 
-        service.add(post)
+        val actual = service.add(post)
+        val expected = post.copy(id = 0)
 
-        val result = posts.last().id
-
-        assertEquals(0, result)
+        assertEquals(expected, actual)
     }
 
     @Test
-    fun update() {
+    fun updateExistingTrue() {
+        val post = Post(
+            id = 0,
+            owner_id = 1,
+            from_id = 2,
+            created_by = 3,
+            date = 2222,
+            text = "Hello",
+            reply_owner_id = 4,
+            reply_post_id = 5,
+            friends_only = true,
+            post_type = "post",
+            signer_id = 6,
+            can_pin = true,
+            can_delete = true,
+            can_edit = true,
+            is_pinned = true,
+            marked_as_ads = true,
+            is_favorite = true,
+            postponed_id = 7
+        )
+
+        val post1 = Post(
+            id = 0,
+            owner_id = 1,
+            from_id = 2,
+            created_by = 3,
+            date = 2222,
+            text = " ",
+            reply_owner_id = 4,
+            reply_post_id = 5,
+            friends_only = true,
+            post_type = "post",
+            signer_id = 6,
+            can_pin = true,
+            can_delete = true,
+            can_edit = true,
+            is_pinned = true,
+            marked_as_ads = true,
+            is_favorite = true,
+            postponed_id = 7
+        )
+        val service = WallService()
+
+        service.add(post)
+        val expected = service.update(post1)
+
+        assertTrue(expected)
+    }
+
+    @Test
+    fun updateExistingFalse() {
+        val post = Post(
+            id = 0,
+            owner_id = 1,
+            from_id = 2,
+            created_by = 3,
+            date = 2222,
+            text = "Hello",
+            reply_owner_id = 4,
+            reply_post_id = 5,
+            friends_only = true,
+            post_type = "post",
+            signer_id = 6,
+            can_pin = true,
+            can_delete = true,
+            can_edit = true,
+            is_pinned = true,
+            marked_as_ads = true,
+            is_favorite = true,
+            postponed_id = 7
+        )
+
+        val post1 = Post(
+            id = 1,
+            owner_id = 1,
+            from_id = 2,
+            created_by = 3,
+            date = 2222,
+            text = " ",
+            reply_owner_id = 4,
+            reply_post_id = 5,
+            friends_only = true,
+            post_type = "post",
+            signer_id = 6,
+            can_pin = true,
+            can_delete = true,
+            can_edit = true,
+            is_pinned = true,
+            marked_as_ads = true,
+            is_favorite = true,
+            postponed_id = 7
+        )
+        val service = WallService()
+
+        service.add(post)
+        val expected = service.update(post1)
+
+        assertFalse(expected)
     }
 }
