532b3a0ae7243cf66a72bee4985de82cb5980bb2
EugeneKhomitskiy <mtl_87@list.ru>
2021-07-05 23:35:12 +0300

    change WallServiceTest.kt

--- src/test/kotlin/ru/netology/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -6,33 +6,70 @@ import org.junit.Assert.*
 
 class WallServiceTest {
 
+    private val post = Post(
+        id = 0,
+        ownerId = 1,
+        fromId = 2,
+        createdBy = 3,
+        date = 2222,
+        text = "Hello",
+        replyOwnerId = 4,
+        replyPostId = 5,
+        friendsOnly = true,
+        comments = Comments(),
+        copyright = Copyright(),
+        likes = Likes(),
+        reposts = Reposts(),
+        views = Views(),
+        postType = "post",
+        signerId = 6,
+        canPin = true,
+        canDelete = true,
+        canEdit = true,
+        isPinned = true,
+        markedAsAds = true,
+        isFavorite = true,
+        postponedId = 7
+    )
+
+    private val post1 = Post(
+        id = 1,
+        ownerId = 1,
+        fromId = 2,
+        createdBy = 3,
+        date = 2222,
+        text = " ",
+        replyOwnerId = 4,
+        replyPostId = 5,
+        friendsOnly = true,
+        comments = Comments(),
+        copyright = Copyright(),
+        likes = Likes(),
+        reposts = Reposts(),
+        views = Views(),
+        postType = "post",
+        signerId = 6,
+        canPin = true,
+        canDelete = true,
+        canEdit = true,
+        isPinned = true,
+        markedAsAds = true,
+        isFavorite = true,
+        postponedId = 7
+    )
+
+    private val comment = Comment(
+        ownerId = 0,
+        postId = 1,
+        fromGroup = 0,
+        message = "Hello",
+        replyToComment = 0,
+        stickerId = 0,
+        guid = " "
+    )
+
     @Test
     fun add_Test() {
-        val post = Post(
-            id = 4,
-            ownerId = 1,
-            fromId = 2,
-            createdBy = 3,
-            date = 2222,
-            text = " ",
-            replyOwnerId = 4,
-            replyPostId = 5,
-            friendsOnly = true,
-            comments = Comments(),
-            copyright = Copyright(),
-            likes = Likes(),
-            reposts = Reposts(),
-            views = Views(),
-            postType = "post",
-            signerId = 6,
-            canPin = true,
-            canDelete = true,
-            canEdit = true,
-            isPinned = true,
-            markedAsAds = true,
-            isFavorite = true,
-            postponedId = 7
-        )
         val service = WallService()
 
         val actual = service.add(post)
@@ -43,118 +80,16 @@ class WallServiceTest {
 
     @Test
     fun updateExistingTrue() {
-        val post = Post(
-            id = 0,
-            ownerId = 1,
-            fromId = 2,
-            createdBy = 3,
-            date = 2222,
-            text = "Hello",
-            replyOwnerId = 4,
-            replyPostId = 5,
-            friendsOnly = true,
-            comments = Comments(),
-            copyright = Copyright(),
-            likes = Likes(),
-            reposts = Reposts(),
-            views = Views(),
-            postType = "post",
-            signerId = 6,
-            canPin = true,
-            canDelete = true,
-            canEdit = true,
-            isPinned = true,
-            markedAsAds = true,
-            isFavorite = true,
-            postponedId = 7
-        )
-
-        val post1 = Post(
-            id = 0,
-            ownerId = 1,
-            fromId = 2,
-            createdBy = 3,
-            date = 2222,
-            text = " ",
-            replyOwnerId = 4,
-            replyPostId = 5,
-            friendsOnly = true,
-            comments = Comments(),
-            copyright = Copyright(),
-            likes = Likes(),
-            reposts = Reposts(),
-            views = Views(),
-            postType = "post",
-            signerId = 6,
-            canPin = true,
-            canDelete = true,
-            canEdit = true,
-            isPinned = true,
-            markedAsAds = true,
-            isFavorite = true,
-            postponedId = 7
-        )
         val service = WallService()
 
         service.add(post)
-        val expected = service.update(post1)
+        val expected = service.update(post1.copy(id = 0))
 
         assertTrue(expected)
     }
 
     @Test
     fun updateExistingFalse() {
-        val post = Post(
-            id = 0,
-            ownerId = 1,
-            fromId = 2,
-            createdBy = 3,
-            date = 2222,
-            text = "Hello",
-            replyOwnerId = 4,
-            replyPostId = 5,
-            friendsOnly = true,
-            comments = Comments(),
-            copyright = Copyright(),
-            likes = Likes(),
-            reposts = Reposts(),
-            views = Views(),
-            postType = "post",
-            signerId = 6,
-            canPin = true,
-            canDelete = true,
-            canEdit = true,
-            isPinned = true,
-            markedAsAds = true,
-            isFavorite = true,
-            postponedId = 7
-        )
-
-        val post1 = Post(
-            id = 1,
-            ownerId = 1,
-            fromId = 2,
-            createdBy = 3,
-            date = 2222,
-            text = " ",
-            replyOwnerId = 4,
-            replyPostId = 5,
-            friendsOnly = true,
-            comments = Comments(),
-            copyright = Copyright(),
-            likes = Likes(),
-            reposts = Reposts(),
-            views = Views(),
-            postType = "post",
-            signerId = 6,
-            canPin = true,
-            canDelete = true,
-            canEdit = true,
-            isPinned = true,
-            markedAsAds = true,
-            isFavorite = true,
-            postponedId = 7
-        )
         val service = WallService()
 
         service.add(post)
@@ -165,90 +100,17 @@ class WallServiceTest {
 
     @Test
     fun shouldNotThrow() {
-        val post = Post(
-            id = 0,
-            ownerId = 1,
-            fromId = 2,
-            createdBy = 3,
-            date = 2222,
-            text = "Hello",
-            replyOwnerId = 4,
-            replyPostId = 5,
-            friendsOnly = true,
-            comments = Comments(),
-            copyright = Copyright(),
-            likes = Likes(),
-            reposts = Reposts(),
-            views = Views(),
-            postType = "post",
-            signerId = 6,
-            canPin = true,
-            canDelete = true,
-            canEdit = true,
-            isPinned = true,
-            markedAsAds = true,
-            isFavorite = true,
-            postponedId = 7
-        )
-        val comment = Comment(
-            ownerId = 0,
-            postId = 0,
-            fromGroup = 0,
-            message = "Hello",
-            replyToComment = 0,
-            stickerId = 0,
-            guid = " "
-        )
         val service = WallService()
 
         service.add(post)
-        service.createComment(comment)
-
-        assert(true)
+        service.createComment(comment.copy(postId = 0))
     }
 
     @Test(expected = PostNotFoundException::class)
     fun shouldThrow() {
-        val post = Post(
-            id = 0,
-            ownerId = 1,
-            fromId = 2,
-            createdBy = 3,
-            date = 2222,
-            text = "Hello",
-            replyOwnerId = 4,
-            replyPostId = 5,
-            friendsOnly = true,
-            comments = Comments(),
-            copyright = Copyright(),
-            likes = Likes(),
-            reposts = Reposts(),
-            views = Views(),
-            postType = "post",
-            signerId = 6,
-            canPin = true,
-            canDelete = true,
-            canEdit = true,
-            isPinned = true,
-            markedAsAds = true,
-            isFavorite = true,
-            postponedId = 7
-        )
-        val comment = Comment(
-            ownerId = 0,
-            postId = 1,
-            fromGroup = 0,
-            message = "Hello",
-            replyToComment = 0,
-            stickerId = 0,
-            guid = " "
-        )
-
         val service = WallService()
 
         service.add(post)
         service.createComment(comment)
-
-        assert(true)
     }
 }
