2eb18af047e863a0faa4e86becd33a6275c382cb
EugeneKhomitskiy <mtl_87@list.ru>
2021-07-05 22:22:07 +0300

    change WallServiceTest.kt

--- src/test/kotlin/ru/netology/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -162,4 +162,93 @@ class WallServiceTest {
 
         assertFalse(expected)
     }
+
+    @Test
+    fun shouldNotThrow() {
+        val post = Post(
+            id = 0,
+            ownerId = 1,
+            fromId = 2,
+            createdBy = 3,
+            date = 2222,
+            text = "Hello",
+            replyOwnerId = 4,
+            replyPostId = 5,
+            friendsOnly = true,
+            comments = Comments(),
+            copyright = Copyright(),
+            likes = Likes(),
+            reposts = Reposts(),
+            views = Views(),
+            postType = "post",
+            signerId = 6,
+            canPin = true,
+            canDelete = true,
+            canEdit = true,
+            isPinned = true,
+            markedAsAds = true,
+            isFavorite = true,
+            postponedId = 7
+        )
+        val comment = Comment(
+            ownerId = 0,
+            postId = 0,
+            fromGroup = 0,
+            message = "Hello",
+            replyToComment = 0,
+            stickerId = 0,
+            guid = " "
+        )
+        val service = WallService()
+
+        service.add(post)
+        service.createComment(comment)
+
+        assert(true)
+    }
+
+    @Test(expected = PostNotFoundException::class)
+    fun shouldThrow() {
+        val post = Post(
+            id = 0,
+            ownerId = 1,
+            fromId = 2,
+            createdBy = 3,
+            date = 2222,
+            text = "Hello",
+            replyOwnerId = 4,
+            replyPostId = 5,
+            friendsOnly = true,
+            comments = Comments(),
+            copyright = Copyright(),
+            likes = Likes(),
+            reposts = Reposts(),
+            views = Views(),
+            postType = "post",
+            signerId = 6,
+            canPin = true,
+            canDelete = true,
+            canEdit = true,
+            isPinned = true,
+            markedAsAds = true,
+            isFavorite = true,
+            postponedId = 7
+        )
+        val comment = Comment(
+            ownerId = 0,
+            postId = 1,
+            fromGroup = 0,
+            message = "Hello",
+            replyToComment = 0,
+            stickerId = 0,
+            guid = " "
+        )
+
+        val service = WallService()
+
+        service.add(post)
+        service.createComment(comment)
+
+        assert(true)
+    }
 }
